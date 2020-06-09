54a895281ebfb77021fc5a4809c41b1c19fe1dfa
Pudding <755969423@qq.com>
2020-06-09 15:30:07 +0800

    edit

--- common/net.js
+++ common/net.js
@@ -10,21 +10,17 @@ const net = function(options) {
 		const token 	= uni.getStorageSync('token');
 		// 如果token存在 请求头链接上token Bearer令牌 设置在请求头
        	if (token) {
-         	options.header = {
-           		'Authorization' : 'Bearer ' + token
-         	};
+         	options.header = { 'Authorization' : 'Bearer ' + token };
         }else{
 			// 如果没有token  定位到登陆页面
-            uni.navigateTo({
-                url: '/pages/index/index' 
-            });
+            uni.navigateTo({ url: '/pages/index/index' });
             return;
         }
      } catch (err) {
 		// 如果请求出错  打印错误信息
       	console.log(err)
     }
-
+	
 	// 返回请求
 	return uni.request(options);
 }
